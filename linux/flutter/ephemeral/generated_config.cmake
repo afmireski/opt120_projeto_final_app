9d4f3d443532271c1f15f85ed962e590f92da482
femedici <ic1dem.contato@gmail.com>
2025-02-05 00:56:55 -0300

    linux flutter build

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,20 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/femedici/Documentos/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/femedici/Documentos/Codes-utf/mobileDev/opt120_projeto_final_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/femedici/Documentos/flutter"
+  "PROJECT_DIR=/home/femedici/Documentos/Codes-utf/mobileDev/opt120_projeto_final_app"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/femedici/Documentos/Codes-utf/mobileDev/opt120_projeto_final_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/femedici/Documentos/Codes-utf/mobileDev/opt120_projeto_final_app/lib/main.dart"
+)
